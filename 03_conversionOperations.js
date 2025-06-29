b6fae2b7d5771ca17b3bb8fd08de27b8698b78b4
Ashwin-SH <ashwinsh052003@gmail.com>
2025-06-29 15:55:20 +0530

    Operations

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -15,8 +15,29 @@
 // console.log(BooleanIsLoggedIn);
 
 
-let a = 1;
-let b = true;
+// let a = 1;
+// let b = true;
 
-console.log(a == b);
-console.log(a === b);
+// console.log(a == b);
+// console.log(a === b);
+
+
+/// OPERATIONS
+
+// console.log(3 + 3)
+// console.log(3 - 3)
+// console.log(3 * 3)
+// console.log(2 ** 3) // IT IS LIKE 2 to the power of 3
+// console.log(3 / 3)
+// console.log(2 % 3) // MODULO OPERATOR GIVES REMAINDER
+
+
+// let x = 10;
+// x++;
+// console.log(x);
+
+
+let a = 10;
+let b = a++;
+console.log(a);
+console.log(b);
