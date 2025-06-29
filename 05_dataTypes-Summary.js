4a0a08a3a24a9b50f73760b8f8811557cb4af831
Ashwin-SH <ashwinsh052003@gmail.com>
2025-06-29 16:32:20 +0530

    data types summary done

---
+++ 05_dataTypes-Summary.js
@@ -0,0 +1,23 @@
+// const id = Symbol('123');
+// const anoterId = Symbol('123');
+
+// console.log(id === anoterId);
+
+// const employees = ['Ashwin', 'Ajeet', 'Akshay', 'KD'];
+// console.log(employees[1]);
+
+// let myObj = {
+//     name: "Ashwin",
+//     age: 25,
+// }
+// console.log(myObj);
+
+// console.log(typeof myObj);
+
+const myFunction = function () {
+  return "Hello World";
+};
+
+console.log("My name is " + myFunction());
+
+console.log(typeof myFunction);
