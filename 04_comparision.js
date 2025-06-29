a11d1b278b9abd8c576faf3502ec9fe1dfabb524
Ashwin-SH <ashwinsh052003@gmail.com>
2025-06-29 16:16:01 +0530

    comparision

---
+++ 04_comparision.js
@@ -0,0 +1,11 @@
+// console.log(1 > 2);
+// console.log(5 > 4);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null === 0);
+console.log(null <= 0);
+
+ 
+//SIMILARLY CHECK FOR UNDEFINED
+
